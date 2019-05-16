de3ae9837fe619e164269c771b0d877f9f30a64f
Renier De La Cruz <me@renierdelacruz.com>
2019-05-16 18:20:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,9 +5,9 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
-    if !BRANDS
-    BRANDS << brand
-    
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end
   end
 
   def cobble
