0e04f2bf888f5699b954ab096a9c5bebb5b1a7f4
Renier De La Cruz <me@renierdelacruz.com>
2019-05-16 18:11:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition, : 
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition, :brand
+  attr_reader
   
   BRANDS = []
   def initialize(brand)
